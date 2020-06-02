77dcea93ef34ca91f596b726839eea870bf7bf7c
Ji Ho Bok <bok.jiho@gmail.com>
2020-06-02 14:59:54 +0900

    front of main page almost done. (need to work on reflexive)

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,10 +1,18 @@
-$('.ui.sidebar')
-  .sidebar('toggle')
-;
 
-$('#main_sidebar').first()
-  .sidebar('attach events', '.open.button', 'show')
-;
-$('.open.button')
-  .removeClass('disabled')
-;
+$(document).ready(function(){
+  $('#menu_button').click(function(){
+    $('#main_sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
+  }); 
+
+  window.onscroll = function(){
+    scrollFunction();
+  }
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+      $('#head_menu').removeClass('fixed');
+    } else {
+      $('#head_menu').addClass('fixed');
+    }
+  }
+});
