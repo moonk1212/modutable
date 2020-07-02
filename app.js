73c1fc8a5d138954a7561ef0fdb2938c5ebd0d0a
Ji Ho Bok <bok.jiho@gmail.com>
2020-07-02 19:23:04 +0900

    mypage front ui done.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/semantic', express.static(__dirname + '/semantic'));
+app.use(express.static(path.join(__dirname, 'node_modules')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('mainpage', { title: '누구나 골목' });
 });
 
+router.get('/about_ngn', function(req, res, next) {
+  res.render('about_ngn', { title: '누구나 골목' });
+});
+
+router.get('/mypage', function(req, res, next){
+  res.render('mypage', { title : '누구나 골목'});
+});
 module.exports = router;
